8d74d0708c874ac84c5b06676e4a454cae29c6f4
Mattia Beccerica <mattiabec@gmail.com>
2021-02-27 14:00:08 +0100

    Aggiunta mvn

---
+++ WeatherCloseRomeApp/src/main/java/it/univpm/WeatherCloseRomeApp/WeatherCloseRomeAppApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.WeatherCloseRomeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCloseRomeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherCloseRomeAppApplication.class, args);
+	}
+
+}
---
+++ WeatherCloseRomeApp/src/test/java/it/univpm/WeatherCloseRomeApp/WeatherCloseRomeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.WeatherCloseRomeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherCloseRomeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
